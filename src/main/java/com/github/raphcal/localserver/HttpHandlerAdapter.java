58e4218c543383463ecc356b09e051e5c22a4e46
Raphaël Calabro <ddaeke-github@yahoo.fr>
2020-09-03 16:37:48 +0200

    Version 2.1.0 Added an adapter to use instances of `com.github.raphcal.localserver.HttpServlet` with `com.sun.net.httpserver.HttpServer`.

---
+++ src/main/java/com/github/raphcal/localserver/HttpHandlerAdapter.java
@@ -0,0 +1,80 @@
+package com.github.raphcal.localserver;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Adapter to ease transition from LocalServer to Sun HttpServer.
+ *
+ * @author Raphaël Calabro (raphael.calabro.external2@banque-france.fr)
+ * @see com.sun.net.httpserver.HttpServer
+ */
+public class HttpHandlerAdapter implements HttpHandler {
+
+    /**
+     * Servlet to adapt into an HttpHandler.
+     */
+    private final HttpServlet servlet;
+
+    /**
+     * Creates a new adapter for the given servlet.
+     *
+     * @param servlet Servlet to adapt into an HttpHandler.
+     */
+    public HttpHandlerAdapter(HttpServlet servlet) {
+        this.servlet = servlet;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        final HttpRequest request = new HttpRequest();
+        final HttpResponse response = new HttpResponse();
+
+        request.setMethod(exchange.getRequestMethod());
+        request.setTarget(exchange.getRequestURI().toString());
+        request.setVersion(exchange.getProtocol());
+        for (Map.Entry<String, List<String>> header : exchange.getRequestHeaders().entrySet()) {
+            request.setHeader(header.getKey(), String.join(";", header.getValue()));
+        }
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(exchange.getRequestBody())) {
+            byte[] result = new byte[4096];
+            int index = 0;
+
+            final byte[] bytes = new byte[2048];
+            int read = bufferedInputStream.read(bytes, 0, bytes.length);
+            while (read != -1) {
+                if (index + read > result.length) {
+                    byte[] copy = new byte[Math.max(index + read * 2, result.length * 2)];
+                    System.arraycopy(result, 0, copy, 0, index);
+                    result = copy;
+                }
+                System.arraycopy(bytes, 0, result, index, read);
+                index += read;
+                read = bufferedInputStream.read(bytes, 0, bytes.length);
+            }
+
+            request.setContent(new String(result, request.getCharset()));
+        }
+
+        servlet.handleRequest(request, response);
+
+        final Headers responseHeaders = exchange.getResponseHeaders();
+        for (Map.Entry<String, String> header : response.getHeaders()) {
+            responseHeaders.add(header.getKey(), header.getValue());
+        }
+        exchange.sendResponseHeaders(response.getStatusCode(), response.getContentLength());
+        try (OutputStream outputStream = exchange.getResponseBody()) {
+            outputStream.write(response.getContentBuilder().toByteArray());
+        }
+    }
+
+}
